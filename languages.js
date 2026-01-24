93f4df8365278a0202c462e9c31514e04ae155b8
estheticode <59923296+estheticode@users.noreply.github.com>
2026-01-24 16:32:32 -0500

    Update languages.js

--- languages.js
+++ languages.js
@@ -1,4 +1,4 @@
-const supportedLanguageCodes = ['en', 'ur', 'fr', 'id', 'es', 'amh','ru', 'sv', 'tr', 'bn', 'zh'];
+const supportedLanguageCodes = ['en', 'ur', 'fr', 'id', 'es', 'am','ru', 'sv', 'tr', 'bn', 'zh'];
 // const supportedLanguageCodes = ['eng', 'urd', 'fra', 'ind', 'spa', 'rus', 'swe', 'tur', 'ben', 'zho'];
 // for 3 letter codes, data files folders have to be renamed accordingly
 console.log('Languages loaded:', supportedLanguageCodes);
